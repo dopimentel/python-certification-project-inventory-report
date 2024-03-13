ee40c92542b05aa43aee632a51447c6fd84cd883
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-12 22:31:12 -0300

    test: req 1 testing class Product constructor

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "123",
+        "product_name",
+        "company_name",
+        "01-01-2021",
+        "01-01-2022",
+        "serial_number",
+        "storage_instructions",
+    )
+    assert product.id == "123"
+    assert product.product_name == "product_name"
+    assert product.company_name == "company_name"
+    assert product.manufacturing_date == "01-01-2021"
+    assert product.expiration_date == "01-01-2022"
+    assert product.serial_number == "serial_number"
+    assert product.storage_instructions == "storage_instructions"
