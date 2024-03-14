4123ce88eed7a7d47f13851e49d7602e3d7c4c50
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-14 11:27:16 -0300

    req 3 Importer interface implemented

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,13 +1,20 @@
+import json
+from abc import ABC, abstractmethod
 from typing import Dict, Type
 
-
-class Importer:
-    pass
+from inventory_report.product import Product
 
 
-class JsonImporter:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str):
+        self.path = path
 
+    @abstractmethod
+    def import_data(self) -> list[Product]:
+        raise NotImplementedError
+
+
+class JsonImporter(Importer):
 
 class CsvImporter:
     pass
@@ -19,3 +26,9 @@ IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
     "csv": CsvImporter,
 }
+
+if __name__ == "__main__":
+    imp = JsonImporter("inventory_report/data/inventory.json")
+    products = imp.import_data()
+    for product in products:
+        print(product)
