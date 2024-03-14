20d879a611c0e4621c5c7227f396cb0137d56c9f
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-13 23:29:03 -0300

    test: req 2 testing class Product magic method __str__

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,19 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "123",
+        "product_name",
+        "company_name",
+        "01-01-2021",
+        "01-01-2022",
+        "serial_number",
+        "storage_instructions",
+    )
+    assert str(product) == (
+        "The product 123 - product_name with serial number serial_number "
+        "manufactured on 01-01-2021 by the company company_name "
+        "valid until 01-01-2022 must be stored according to the following "
+        "instructions: storage_instructions."
+    )
