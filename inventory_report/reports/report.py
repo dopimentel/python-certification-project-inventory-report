8d107450c4e374584b5792720c26fdd2478c91f3
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-20 18:53:26 -0300

    req 6 Report Protocol

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,9 @@
-class Report:
-    pass
+from typing import Protocol
+
+
+class Report(Protocol):
+    def generate(self) -> str:
+        raise NotImplementedError
+
+    def add_inventory(self, inventory) -> None:
+        raise NotImplementedError
