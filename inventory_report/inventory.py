fd3982c02cb2dbb337f70e5a067033c14e9ea41b
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-20 18:13:46 -0300

    req 5 class Inventory implemented

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from typing import Union
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    @property
+    def data(self) -> list[Product]:
+        return self.__data.copy()
+
+    def __init__(self, data: Union[list[Product], None] = None) -> None:
+        self.__data = data or []
+
+    def add_data(self, data: list[Product]) -> None:
+        self.__data.extend(data)
