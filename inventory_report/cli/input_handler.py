82ccfd65de0ff2b5a2c2ea16a81d65e2e637e408
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-21 10:33:07 -0300

    req 10 implements function to handle CLI inputs

--- inventory_report/cli/input_handler.py
+++ inventory_report/cli/input_handler.py
@@ -1,9 +1,26 @@
 from typing import List
 
+from inventory_report.importers import CsvImporter, JsonImporter
+from inventory_report.inventory import Inventory
+from inventory_report.reports import REPORTS
+
 
 def process_report_request(file_paths: List[str], report_type: str) -> str:
     """
     Process the report given a list of file paths and a report type,
     and returns the result.
     """
-    raise NotImplementedError
+    if report_type in REPORTS:
+        report = REPORTS[report_type]()
+        for file in file_paths:
+            if file.endswith(".csv"):
+                list_products = CsvImporter(file).import_data()
+                inventory = Inventory(list_products)
+                report.add_inventory(inventory)
+            if file.endswith(".json"):
+                list_products = JsonImporter(file).import_data()
+                inventory = Inventory(list_products)
+                report.add_inventory(inventory)
+        return report.generate()
+
+    raise ValueError("Report type is invalid.")
